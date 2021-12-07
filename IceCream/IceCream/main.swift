ad3cb4e2d7aec13157440f72b17b86db49d54916
kio <un19451024@gmail.com>
2021-12-07 09:21:40 +0900

    Feat: 아이스크림 구현

---
+++ IceCream/IceCream/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  IceCream
+//
+//  Created by kio on 2021/12/07.
+//
+
+import Foundation
+
+for index in 1...11 {
+    print("***********")
+}
+
+for index in 1...4 {
+    print("    | |")
+}
+
