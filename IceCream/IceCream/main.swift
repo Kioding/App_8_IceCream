2d3a096a6a3d1afec206521cca1d9e565d4bfa3a
kio <un19451024@gmail.com>
2021-12-07 09:31:57 +0900

    Feat: 아이스크림 class 생성 -  길이, 몸통, 토핑, 막대길이 변수 생성

--- IceCream/IceCream/main.swift
+++ IceCream/IceCream/main.swift
@@ -7,17 +7,33 @@
 
 import Foundation
 
-func drawPartOfIceCream() {
-    for index in 1...11 {
-        print("***********")
+class IceCream {
+    var lengthOfIceCream: Int
+    var IceCreamBody: String
+    var topping: String
+    var lengthOfIceCreamBar: Int
+    
+    init(lengthOfIceCream:Int, IceCreamBody: String, topping: String = " ", lengthOfIceCreamBar: Int) {
+        self.lengthOfIceCream = lengthOfIceCream
+        self.IceCreamBody = IceCreamBody
+        self.topping = topping
+        self.lengthOfIceCreamBar = lengthOfIceCreamBar
+    }
+    
+    func drawPartOfIceCream() {
+        for index in 1...11 {
+            print("***********")
+        }
+    }
+    
+    func drawPartOfIceCreamBar() {
+        for index in 1...4 {
+            print("    | |")
+        }
     }
 }
 
-func drawPartOfIceCreamBar() {
-    for index in 1...4 {
-        print("    | |")
-    }
-}
+let OrderedIceCream = IceCream(lengthOfIceCream: 10, IceCreamBody: "***", topping: " ", lengthOfIceCreamBar: 4)
 
-drawPartOfIceCream()
-drawPartOfIceCreamBar()
+OrderedIceCream.drawPartOfIceCream()
+OrderedIceCream.drawPartOfIceCreamBar()
