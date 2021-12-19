1c3a6b8b466fe5fe6f80b9823cff4be62710797e
Kio <un19451024@gmail.com>
2021-12-19 18:26:39 +0900

    Chore: IceCream Class 파일 분리

---
+++ IceCream/IceCream/IceCream.swift
@@ -0,0 +1,63 @@
+//
+//  IceCream.swift
+//  IceCream
+//
+//  Created by kio on 2021/12/19.
+//
+
+import Foundation
+
+class IceCream {
+    var lengthOfIceCream: Int
+    var iceCreamBody: String
+    var topping: String
+    var lengthOfIceCreamBar: Int
+    
+    init(lengthOfIceCream:Int, IceCreamBody: String, topping: String = " ", lengthOfIceCreamBar: Int) {
+        self.lengthOfIceCream = lengthOfIceCream
+        self.iceCreamBody = IceCreamBody
+        self.topping = topping
+        self.lengthOfIceCreamBar = lengthOfIceCreamBar
+    }
+    
+    func makeIceCream() {
+        showInformationOfIceCream()
+        drawPartOfIceCream()
+        drawPartOfIceCreamBar()
+    }
+    
+    func showInformationOfIceCream() {
+        print("""
+              <정보>
+              길이: \(lengthOfIceCream)
+              몸통: \(iceCreamBody)
+              토핑: \(topping)
+              막대길이: \(lengthOfIceCreamBar)
+              
+              """)
+    }
+    
+    func drawPartOfIceCream() {
+        for index in 0..<lengthOfIceCream {
+            if index % 2 == 0 {
+                print("\(self.topping+self.iceCreamBody)")
+            } else {
+                print(" \(self.iceCreamBody+self.topping)")
+            }
+        }
+    }
+    
+    func drawPartOfIceCreamBar() {
+        let spacing: Int = self.iceCreamBody.count / 2
+        var spacingLength: String = ""
+        let shapeOfIceCreamBar: String = "| |"
+        
+        for _ in 0..<spacing {
+            spacingLength += " "
+        }
+        
+        for _ in 0..<self.lengthOfIceCreamBar {
+            print("\(spacingLength+shapeOfIceCreamBar)")
+        }
+    }
+}
--- IceCream/IceCream/main.swift
+++ IceCream/IceCream/main.swift
@@ -7,62 +7,11 @@
 
 import Foundation
 
-class IceCream {
-    var lengthOfIceCream: Int
-    var iceCreamBody: String
-    var topping: String
-    var lengthOfIceCreamBar: Int
-    
-    init(lengthOfIceCream:Int, IceCreamBody: String, topping: String = " ", lengthOfIceCreamBar: Int) {
-        self.lengthOfIceCream = lengthOfIceCream
-        self.iceCreamBody = IceCreamBody
-        self.topping = topping
-        self.lengthOfIceCreamBar = lengthOfIceCreamBar
-    }
-    
-    func makeIceCream() {
-        showInformationOfIceCream()
-        drawPartOfIceCream()
-        drawPartOfIceCreamBar()
-    }
-    
-    func showInformationOfIceCream() {
-        print("""
-              <정보>
-              길이: \(lengthOfIceCream)
-              몸통: \(iceCreamBody)
-              토핑: \(topping)
-              막대길이: \(lengthOfIceCreamBar)
-              
-              """)
-    }
-    
-    func drawPartOfIceCream() {
-        for index in 0..<lengthOfIceCream {
-            if index % 2 == 0 {
-                print("\(self.topping+self.iceCreamBody)")
-            } else {
-                print(" \(self.iceCreamBody+self.topping)")
-            }
-        }
-    }
-    
-    func drawPartOfIceCreamBar() {
-        let spacing: Int = self.iceCreamBody.count / 2
-        var spacingLength: String = ""
-        let shapeOfIceCreamBar: String = "| |"
-        
-        for _ in 0..<spacing {
-            spacingLength += " "
-        }
-        
-        for _ in 0..<self.lengthOfIceCreamBar {
-            print("\(spacingLength+shapeOfIceCreamBar)")
-        }
-    }
-}
+// iceCream Order
+let fristIceCream = IceCream(lengthOfIceCream: 6, IceCreamBody: "|#$%&&%$#|", topping: "*", lengthOfIceCreamBar: 3)
+let secondIceCream = IceCream(lengthOfIceCream: 8, IceCreamBody: "***", topping: "#", lengthOfIceCreamBar: 3)
+let thirdIceCream = IceCream(lengthOfIceCream: 6, IceCreamBody: "|0|", lengthOfIceCreamBar: 3)
 
-// iceCream Sample
-let OrderedIceCream = IceCream(lengthOfIceCream: 10, IceCreamBody: "|#$%&&%$#|", topping: "*", lengthOfIceCreamBar: 4)
-
-OrderedIceCream.makeIceCream()
+fristIceCream.makeIceCream()
+secondIceCream.makeIceCream()
+thirdIceCream.makeIceCream()
